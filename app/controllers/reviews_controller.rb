577b87b000f845da294b04944b8a2a3adf9549cc
Nathan Germond <germond.nathan@gmail.com>
2024-02-21 16:13:33 +0100

    end

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,26 @@
 class ReviewsController < ApplicationController
-  before_action :set_review, only: %i[destroy]
+  before_action :set_review, only: %i[destroy show]
   def destroy
     @review.destroy
   end
 
   def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
-    @review.save
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
+  def new
+    @review = Review.new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def show; end
 
   private
 
@@ -16,6 +29,6 @@ class ReviewsController < ApplicationController
   end
 
   def review_params
-    params.require(:review).permit(:content)
+    params.require(:review).permit(:content, :rating)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_21_102937) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_21_134224) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.text "address"
@@ -25,6 +25,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_21_102937) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "restaurant_id"
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
 
+  add_foreign_key "reviews", "restaurants"
 end
